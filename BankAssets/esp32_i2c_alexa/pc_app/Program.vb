1d3dc05589262a195fb75a946087ac08ef53794c
herrtechniker <83061806+HerrTechniker@users.noreply.github.com>
2026-01-09 14:32:02 +0100

    Remove ApplicationConfiguration usage in VB app

--- BankAssets/esp32_i2c_alexa/pc_app/Program.vb
+++ BankAssets/esp32_i2c_alexa/pc_app/Program.vb
@@ -4,7 +4,6 @@ Imports System.Windows.Forms
 Module Program
     <STAThread>
     Sub Main()
-        ApplicationConfiguration.Initialize()
         Application.Run(New MainForm())
     End Sub
 End Module
